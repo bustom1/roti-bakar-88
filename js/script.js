0cac89ee8fcfc878610200463ddbe582f89a8621
nandhastr <nandhastr2@gmail.com>
2023-10-05 21:29:00 +0700

    tambah style di button produk

--- js/script.js
+++ js/script.js
@@ -1,36 +1,4 @@
 $(document).ready(function () {
-    //section product
-    var bg_produk = "#FEFBC7";
-    $(".product_section").css({
-        "background-color": bg_produk,
-        margin: "5px 0px 0px 0px",
-        padding: "20px 0px 15px 0px",
-    });
-    $(".tombol").css({
-        "background-color": bg_produk,
-        color: "black",
-        "border-radius": "15px",
-    });
-    $(".tombol, .tombol-order").css({
-        color: "black",
-        "border-radius": "15px",
-        "margin-left": "10px",
-        "box-shadow": "3px 3px 9px black",
-    });
-    $(".tombol, .tombol-order, img, .a-tab").hover(
-        function () {
-            $(this).css({
-                transform: "scale(1.1)",
-                transition: "0.2s",
-            });
-        },
-        function () {
-            $(this).css({
-                transform: "scale(1)",
-            });
-        }
-    );
-
     // show modal pop up
     $(".tombol").click(function () {
         var modalContent = `
@@ -174,13 +142,13 @@ function allProduct() {
                             <!-- endModal -->
                             <button
                                 type="button"
-                                class="btn tombol btn-success m-md-2"
+                                class="btn tombol  m-md-2"
                                 data-bs-toggle="modal"
                                 data-bs-target="#detailModal"
                                 onclick="showDetail('${dataProduct.img}','${dataProduct.name}', '${dataProduct.price}')"
                             >Lihat Detail</button>
 
-                            <a href="https://api.whatsapp.com/send?phone=6285646044393&text=Saya%20Pesan%20${dataProduct.name}%20jumlah%20satu%20harga%20Rp.${dataProduct.price}" class="btn tombol-order btn-success">Order</a>
+                            <a href="https://api.whatsapp.com/send?phone=6285646044393&text=Saya%20Pesan%20${dataProduct.name}%20jumlah%20satu%20harga%20Rp.${dataProduct.price}" class="btn tombol-order ">Order <i class="fa-brands fa-whatsapp"></i> </a>
                         </div>
                     </div>
                 </div>`;
